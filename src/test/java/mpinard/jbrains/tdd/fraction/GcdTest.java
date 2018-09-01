9554320b206c11fad02b01142ec09653b96c694c
Michael Pinard <mike.pinard@adp.com>
2018-08-31 21:27:22 -0400

    Added more tests for GCD of 1 and another number

--- src/test/java/mpinard/jbrains/tdd/fraction/GcdTest.java
+++ src/test/java/mpinard/jbrains/tdd/fraction/GcdTest.java
@@ -12,6 +12,10 @@ public class GcdTest {
     @Test
     public void Given_OneAndAnyNumber_Then_GcdIsOne() {
         assertThat(gcd(1, 1)).isEqualTo(1);
+        assertThat(gcd(1, 3)).isEqualTo(1);
+        assertThat(gcd(3, 1)).isEqualTo(1);
+        assertThat(gcd(12, 1)).isEqualTo(1);
+        assertThat(gcd(1, 12)).isEqualTo(1);
     }
     
     private static int gcd(final int value1, final int value2) {
